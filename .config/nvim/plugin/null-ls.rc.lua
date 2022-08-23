268d1e5300fb975c396a98bd5661b796cd96ab4d
Gustavo Santos <gustavofsantos@outlook.com>
2022-08-22 22:51:22 -0300

    update settings

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -2,6 +2,7 @@ local status, null_ls = pcall(require, "null-ls")
 if (not status) then return end
 
 null_ls.setup {
+  debounce = 300,
   sources = {
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.diagnostics.fish,
