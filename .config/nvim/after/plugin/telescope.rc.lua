e0af9dd10aef9f96ec7955a153c0c8d367ae00ac
Gustavo Santos <gustavo.fernandes@loggi.com>
2022-09-05 09:59:55 -0300

    configure fzf

--- .config/nvim/after/plugin/telescope.rc.lua
+++ .config/nvim/after/plugin/telescope.rc.lua
@@ -3,13 +3,19 @@ if (not telescope) then return end
 
 telescope.setup {
   defaults = {
-    dynamic_preview_title = true
+    dynamic_preview_title = true,
   },
   extensions = {
     ["ui-select"] = {
-      require('telescope.themes').get_dropdown { }
+      require('telescope.themes').get_dropdown {}
+    },
+    fzf = {
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
     }
-  }
+  },
 }
 
 telescope.load_extension('harpoon')
