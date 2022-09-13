e74d40c83380d32e0f39d5838ab43cbbc5abe012
Gustavo Santos <gustavo.fernandes@loggi.com>
2022-09-13 11:31:32 -0300

    update settings

--- .config/nvim/after/plugin/cmp.rc.lua
+++ .config/nvim/after/plugin/cmp.rc.lua
@@ -2,6 +2,13 @@ local cmp = require('cmp')
 local lspkind = require('lspkind')
 if (not cmp) then return end
 
+local luasnip = require('luasnip')
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -12,6 +19,26 @@ cmp.setup({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
@@ -22,19 +49,20 @@ cmp.setup({
     { name = 'luasnip' },
     { name = 'nvim_lua ' },
     { name = 'nvim_lsp' },
-    { name = 'buffer' },
+  }, {
+    { name = 'buffer' }
   }),
   formatting = {
     format = lspkind.cmp_format({
-      with_text = false,
+      with_text = true,
       mode = 'symbol', -- show only symbol annotations
       maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
       menu = {
+        luasnip = "[snip]",
         buffer = "[buf]",
         nvim_lsp = "[LSP]",
         nvim_lua = "[api]",
         path = "[path]",
-        luasnip = "[snip]",
       }
     })
   }
--- .config/nvim/after/plugin/luasnip.rc.lua
+++
@@ -1,4 +0,0 @@
-local status, vscode_loader = pcall(require, 'luasnip.loaders.from_vscode')
-if (not status) then return end
-
-vscode_loader.lazy_load { paths = { "./dotfiles-public/.config/nvim/snippets" } }
