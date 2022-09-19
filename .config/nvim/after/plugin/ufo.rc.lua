a9917c1da67284e6b3d59eaa58514d4930e4b887
Gustavo Santos <gustavofsantos@outlook.com>
2022-09-19 10:02:29 -0300

    remove ufo config

--- .config/nvim/after/plugin/ufo.rc.lua
+++
@@ -1,8 +0,0 @@
-local status, ufo = pcall(require, 'ufo')
-if (not status) then return end
-
--- ufo.setup({
---   provider_selector = function(bufnr, filetype, buftype)
---     return { 'treesitter', 'indent' }
---   end
--- })
