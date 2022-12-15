2ceb603c87ac6abee33deadcb06e744ea41222a5
Gustavo Santos <gustavo.fernandes@loggi.com>
2022-12-15 17:52:58 -0300

    enable gitsigns on null-ls

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -6,7 +6,7 @@ null_ls.setup {
   sources = {
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.diagnostics.fish,
-    -- null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.formatting.prettier
   }
 }
