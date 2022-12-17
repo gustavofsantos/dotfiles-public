84b6b53e491a4ba531a03b11cc26dcc0ca1ec5ba
Gustavo Santos <gustavofsantos@outlook.com>
2022-12-17 19:50:21 -0300

    update signs

--- .config/nvim/after/plugin/gitsigns.rc.lua
+++ .config/nvim/after/plugin/gitsigns.rc.lua
@@ -7,4 +7,11 @@ gitsigns.setup {
   attach_to_untracked = false,
   current_line_blame = true,
   current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d>',
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  }
 }
