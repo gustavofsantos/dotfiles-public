e9781fd6f469d9998337802de80ef75c3e013ed4
Gustavo Santos <gustavo.fernandes@loggi.com>
2022-12-20 09:15:21 -0300

    update harpoon marks

--- .config/nvim/after/plugin/harpoon.rc.lua
+++ .config/nvim/after/plugin/harpoon.rc.lua
@@ -4,5 +4,7 @@ if (not harpoon) then return end
 harpoon.setup {  }
 
 -- mark file with harpoon
-vim.keymap.set("n", "<leader>m", require('harpoon.mark').add_file)
-vim.keymap.set("n", "<leader>fm", ":Telescope harpoon marks<CR>") -- find marks
+vim.keymap.set("n", "<leader>am", require('harpoon.mark').add_file)
+vim.keymap.set("n", "<leader>]m", require('harpoon.ui').nav_next, { desc = "Next [M]ark" })
+vim.keymap.set("n", "<leader>[m", require('harpoon.ui').nav_prev, { desc = "Previous [M]ark" })
+vim.keymap.set("n", "<leader>fm", require("harpoon.ui").toggle_quick_menu, { desc = "[F]ind [M]arks" }) -- find marks
