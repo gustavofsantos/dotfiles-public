f5d81b8e434eae3d4f9724a0d55ddd93ea089a61
Gustavo Santos <gustavo.fernandes@loggi.com>
2022-12-20 09:31:07 -0300

    move nvim-tree settings to navigation.rc.lua

--- .config/nvim/after/plugin/navigation.rc.lua
+++ .config/nvim/after/plugin/navigation.rc.lua
@@ -62,3 +62,60 @@ if (has_harpoon) then
   vim.keymap.set("n", "<leader>fm", require("harpoon.ui").toggle_quick_menu, { desc = "[F]ind [M]arks" }) -- find marks
 end
 
+-- Setup tree view
+local has_nvimtree, nvimtree = pcall(require, 'nvim-tree')
+if (has_nvimtree) then
+  local gwidth = vim.api.nvim_list_uis()[1].width
+  local gheight = vim.api.nvim_list_uis()[1].height
+  local width = 80
+  local height = 30
+
+  nvimtree.setup {
+    respect_buf_cwd = true,
+    view = {
+      -- width = 36,
+      float = {
+        enable = true,
+        quit_on_focus_loss = true,
+        open_win_config = {
+          relative = "editor",
+          -- border = "rounded",
+          width = width,
+          height = height,
+          row = (gheight - height) * 0.4,
+          col = (gwidth - width) * 0.5,
+        },
+      }
+    },
+    actions = {
+      open_file = {
+        quit_on_open = true,
+      }
+    },
+    renderer = {
+      highlight_git = true,
+      icons = {
+        show = {
+          file = true,
+          folder = true
+        }
+      },
+      special_files = { "package.json", "Makefile", "README.md", "readme.md" },
+    },
+    update_focused_file = {
+      enable = true,
+      update_root = false,
+      ignore_list = {},
+    },
+    diagnostics = {
+      enable = true,
+      show_on_dirs = false,
+      icons = {
+        hint = "",
+        info = "",
+        warning = "",
+        error = "",
+      },
+    },
+  }
+end
