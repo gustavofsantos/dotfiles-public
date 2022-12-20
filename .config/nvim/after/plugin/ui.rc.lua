5418fe34a42719964311141cc6d764b5dd515fcc
Gustavo Santos <gustavo.fernandes@loggi.com>
2022-12-20 09:34:32 -0300

    move bufferline and lualine settings to ui.rc.lua

---
+++ .config/nvim/after/plugin/ui.rc.lua
@@ -0,0 +1,61 @@
+local has_bufferline, bufferline = pcall(require, 'bufferline')
+if (has_bufferline) then
+  bufferline.setup({
+    options = {
+      mode = "tabs",
+      -- separator_style = 'thin',
+      always_show_bufferline = false,
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+      color_icons = true,
+      diagnostics = "nvim_lsp",
+      indicator = {
+        icon = '▎', -- this should be omitted if indicator style is not 'icon'
+        style = 'icon',
+      },
+      -- offsets = {
+      --   {
+      --     filetype = "NvimTree",
+      --     text = "File Explorer",
+      --     highlight = "Directory",
+      --     text_align = "left"
+      --   }
+      -- }
+    },
+  })
+end
+
+-- Setup lualine
+local has_lualine, lualine = pcall(require, 'lualine')
+if (has_lualine) then
+  lualine.setup({
+    options = {
+      icons_enabled = true,
+      theme = 'auto',
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+      always_divide_middle = true,
+      globalstatus = false,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+      }
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = {},
+      lualine_c = {
+        {
+          'filename',
+          file_status = true,
+          path = 0
+        }
+      },
+      lualine_x = { 'diagnostics' },
+      lualine_y = { 'branch' },
+      lualine_z = {}
+    },
+    extensions = { 'quickfix', 'nvim-tree', 'toggleterm' }
+  })
+end
