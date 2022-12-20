6c7fbb6e6975d8dc98913a5ccec03ac8cd85a0af
Gustavo Santos <gustavo.fernandes@loggi.com>
2022-12-20 10:07:59 -0300

    move null-ls settings

--- .config/nvim/after/plugin/code.rc.lua
+++ .config/nvim/after/plugin/code.rc.lua
@@ -204,3 +204,16 @@ if has_treesitter then
     },
   }
 end
+
+local has_null_ls, null_ls = pcall(require, "null-ls")
+if has_null_ls then
+  null_ls.setup {
+    debounce = 300,
+    sources = {
+      null_ls.builtins.diagnostics.eslint,
+      null_ls.builtins.diagnostics.fish,
+      null_ls.builtins.code_actions.gitsigns,
+      null_ls.builtins.formatting.prettier
+    }
+  }
+end
