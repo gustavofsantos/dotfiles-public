135b23c2a75b99074f7f1191058baf23221f6904
Gustavo Santos <gustavo.fernandes@loggi.com>
2022-12-20 09:41:57 -0300

    setup git

--- .config/nvim/after/plugin/git.rc.lua
+++ .config/nvim/after/plugin/git.rc.lua
@@ -1,7 +1,8 @@
-local status, conflict = pcall(require, 'git-conflict')
-if (not status) then return end
+local has_conflict, conflict = pcall(require, 'git-conflict')
+if (has_conflict) then
+  conflict.setup {}
+end
 
-conflict.setup {}
 --[[
   Mappings
     co - choose ours
@@ -11,3 +12,26 @@ conflict.setup {}
     ]x - move to previous conflict
     [x - move to next conflict
 --]]
+
+local has_gitsigns, gitsigns = pcall(require, 'gitsigns')
+if (has_gitsigns) then
+  gitsigns.setup {
+    signcolumn = true,
+    numhl = false,
+    attach_to_untracked = false,
+    current_line_blame = true,
+    current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d>',
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    }
+  }
+end
+
+local has_diffview, diffview = pcall(require, 'diffview')
+if (has_diffview) then
+  diffview.setup {}
+end
