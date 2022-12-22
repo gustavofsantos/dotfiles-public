22db049ce0953ffba6b55c1eebde2be3e0653b1b
Gustavo Santos <gustavo.fernandes@loggi.com>
2022-12-22 10:01:26 -0300

    add bootstrap script

---
+++ bin/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo ">> Installing dependencies"
+sudo apt install -y automake lzma pcre2-utils
+
+# Install asdf
+echo ">> Installing ASDF"
+
+# Install asdf plugins
+echo ">> Installing ASDF plugins"
+asdf plugin add nodejs
+asdf plugin add exa
+asdf plugin add fzf
+asdf plugin add golang
+asdf plugin-add jq https://github.com/AZMCode/asdf-jq.git
