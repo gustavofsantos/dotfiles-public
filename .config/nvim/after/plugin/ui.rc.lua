e5ee0044d9b31ed551ddf8c9d2faa1ecf2c900f7
Gustavo Santos <gustavo.fernandes@loggi.com>
2023-01-05 13:58:14 -0300

    update themes

--- .config/nvim/after/plugin/ui.rc.lua
+++ .config/nvim/after/plugin/ui.rc.lua
@@ -32,8 +32,8 @@ if (has_lualine) then
     options = {
       icons_enabled = true,
       theme = 'auto',
-      component_separators = { left = '', right = '' },
-      section_separators = { left = '', right = '' },
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
       always_divide_middle = true,
       globalstatus = false,
       refresh = {
