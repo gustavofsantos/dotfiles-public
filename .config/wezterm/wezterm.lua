4ba6a7c5087d6da6a11a51d2934443aa7030f780
Gustavo Santos <gustavo.fernandes@loggi.com>
2023-01-20 09:16:29 -0300

    update wezterm settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,37 +2,18 @@ local wezterm = require("wezterm")
 
 function scheme_for_appearance(appearance)
   if appearance:find "Dark" then
-    -- return "Catppuccin Mocha"
-    return "Kanagawa"
+    return "duskfox"
   else
-    return "Catppuccin Latte"
+    return "dayfox"
   end
 end
 
 return {
-  -- color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
   force_reverse_video_cursor = true,
-  colors = {
-    foreground = "#dcd7ba",
-    background = "#1f1f28",
-
-    cursor_bg = "#c8c093",
-    cursor_fg = "#c8c093",
-    cursor_border = "#c8c093",
-
-    selection_fg = "#c8c093",
-    selection_bg = "#2d4f67",
-
-    scrollbar_thumb = "#16161d",
-    split = "#16161d",
-
-    ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-    brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-    indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-  },
+  color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 
   -- font
-  font = wezterm.font("MonoLisaCustom Nerd Font", { weight = "Regular" }),
+  font = wezterm.font("MonoLisa Nerd Font", { weight = "Regular" }),
   font_size = 11,
 
   window_background_opacity = 0.98,
