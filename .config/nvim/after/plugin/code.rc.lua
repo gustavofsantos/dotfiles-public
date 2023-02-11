d08c9aaa81b7b14f9e6caddad4394e0a02c1f42a
Gustavo Santos <gustavofsantos@outlook.com>
2023-02-11 15:48:46 -0300

    disable autocomplete

--- .config/nvim/after/plugin/code.rc.lua
+++ .config/nvim/after/plugin/code.rc.lua
@@ -23,6 +23,12 @@ local has_lspkind, lspkind = pcall(require, 'lspkind')
 local has_luasnip, luasnip = pcall(require, 'luasnip')
 if (has_cmp and has_lspkind and has_luasnip) then
   cmp.setup({
+    performance = {
+      debounce = 300,
+    },
+    completion = {
+      autocomplete = false
+    },
     snippet = {
       expand = function(args)
         require('luasnip').lsp_expand(args.body)
@@ -31,7 +37,7 @@ if (has_cmp and has_lspkind and has_luasnip) then
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-Space>'] = cmp.mapping.complete({ reason = cmp.ContextReason.Auto }),
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
