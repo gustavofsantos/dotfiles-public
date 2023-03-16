fa5349ed1b83a3db22a0d19515899ad9b5de24da
Gustavo Santos <gustavo.fernandes@loggi.com>
2023-03-16 11:55:05 -0300

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 function scheme_for_appearance(appearance)
-  return "NightOwl"
+  return "nord"
 
   -- if appearance:find "Dark" then
   --   return "Catppuccin Frappe"
