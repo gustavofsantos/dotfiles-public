fd3876ba6242c48297271e34c39fcc0028008355
Gustavo Santos <gustavofsantos@outlook.com>
2023-06-14 19:10:27 -0300

    improve lsp setup

--- .config/nvim/after/plugin/lsp.rc.lua
+++ .config/nvim/after/plugin/lsp.rc.lua
@@ -2,13 +2,6 @@ vim.diagnostic.config({
   virtual_text = true,
 })
 
-local has_luasnip, _ = pcall(require, 'luasnip')
-if has_luasnip then
-  require("luasnip.loaders.from_vscode").lazy_load {
-    paths = { "~/.config/nvim/snippets" }
-  }
-end
-
 local has_fidget, fidget = pcall(require, 'fidget')
 if (has_fidget) then
   fidget.setup()
@@ -32,10 +25,14 @@ if (has_lsp) then
   lsp.setup()
 end
 
-local has_cmp, cmp = pcall(require, 'cmp')
+local has_cmp, _ = pcall(require, 'cmp')
 if (has_cmp) then
   local cmp = require('cmp')
   local cmp_action = require('lsp-zero').cmp_action()
+
+  require("luasnip.loaders.from_vscode").lazy_load({
+    paths = { "~/.config/nvim/snippets" }
+  })
 
   cmp.setup({
     sources = {
