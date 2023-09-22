203dda2a23931b1926b59e5ffb209b74de7e379c
Gustavo Santos <gustavofsantos@outlook.com>
2023-09-22 18:59:41 -0300

    ignore dap

--- .config/nvim/after/plugin/code.rc.lua
+++ .config/nvim/after/plugin/code.rc.lua
@@ -1,136 +1,136 @@
 vim.o.foldcolumn = "0" -- '0' is not bad
-vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
 local has_treesitter, treesitter = pcall(require, "nvim-treesitter.configs")
 if not has_treesitter then
-  return
+    return
 end
 
 treesitter.setup({
-  -- A list of parser names, or "all"
-  ensure_installed = {
-    "gitcommit",
-    "git_rebase",
-    "bash",
-    "go",
-    "lua",
-    "json",
-    "proto",
-    "toml",
-    "rust",
-    "haskell",
-    "markdown",
-    "javascript",
-    "jsdoc",
-    "typescript",
-    "tsx",
-    "prisma",
-    "python",
-    "vim",
-    "elixir",
-    "css",
-    "eex",
-    "heex",
-    "svelte",
-  },
-  highlight = {
-    enable = true,
-    use_languagetree = true,
-    additional_vim_regex_highlighting = { "markdown" },
-  },
-  indent = {
-    enable = true,
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<c-space>",
-      node_incremental = "<c-space>",
-      scope_incremental = "<c-s>",
-      node_decremental = "<c-backspace>",
+    -- A list of parser names, or "all"
+    ensure_installed = {
+        "gitcommit",
+        "git_rebase",
+        "bash",
+        "go",
+        "lua",
+        "json",
+        "proto",
+        "toml",
+        "rust",
+        "haskell",
+        "markdown",
+        "javascript",
+        "jsdoc",
+        "typescript",
+        "tsx",
+        "prisma",
+        "python",
+        "vim",
+        "elixir",
+        "css",
+        "eex",
+        "heex",
+        "svelte",
     },
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["aa"] = "@parameter.outer",
-        ["ia"] = "@parameter.inner",
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
-      },
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+        additional_vim_regex_highlighting = { "markdown" },
     },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer",
-      },
-      goto_next_end = {
-        ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
-      },
-      goto_previous_start = {
-        ["[m"] = "@function.outer",
-        ["[["] = "@class.outer",
-      },
-      goto_previous_end = {
-        ["[M"] = "@function.outer",
-        ["[]"] = "@class.outer",
-      },
+    indent = {
+        enable = true,
     },
-    swap = {
-      enable = true,
-      swap_next = {
-        ["<leader>a"] = "@parameter.inner",
-      },
-      swap_previous = {
-        ["<leader>A"] = "@parameter.inner",
-      },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<c-space>",
+            node_incremental = "<c-space>",
+            scope_incremental = "<c-s>",
+            node_decremental = "<c-backspace>",
+        },
     },
-  },
-  context_commentstring = {
-    enable = true,
-  },
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["aa"] = "@parameter.outer",
+                ["ia"] = "@parameter.inner",
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["]m"] = "@function.outer",
+                ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+                ["]M"] = "@function.outer",
+                ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+                ["[m"] = "@function.outer",
+                ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+                ["[M"] = "@function.outer",
+                ["[]"] = "@class.outer",
+            },
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+                ["<leader>A"] = "@parameter.inner",
+            },
+        },
+    },
+    context_commentstring = {
+        enable = true,
+    },
 })
 
 require("nvim-treesitter.configs").setup({
-  autotag = {
-    enable = true,
-  },
+    autotag = {
+        enable = true,
+    },
 })
 
 local has_neotest, _ = pcall(require, "neotest")
 if has_neotest then
-  require("neotest").setup({
-    adapters = {
-      require("neotest-python")({
-        dap = { justMyCode = false },
-        pytest_discovery = true,
-      }),
-      require("neotest-jest")({
-        jestCommand = "npm test -- ",
-      }),
-      require("neotest-vitest"),
-      require("neotest-playwright").adapter({
-        options = {
-          preset = "headed",
-          enable_dynamic_test_discovery = true,
+    require("neotest").setup({
+        adapters = {
+            require("neotest-python")({
+                dap = { justMyCode = false },
+                pytest_discovery = true,
+            }),
+            require("neotest-jest")({
+                jestCommand = "npm test -- ",
+            }),
+            require("neotest-vitest"),
+            require("neotest-playwright").adapter({
+                options = {
+                    preset = "headed",
+                    enable_dynamic_test_discovery = true,
+                },
+            }),
         },
-      }),
-    },
-    consumers = {
-      playwright = require("neotest-playwright.consumers").consumers,
-    },
-    icons = {
-      running_animated = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
-    },
-  })
+        consumers = {
+            playwright = require("neotest-playwright.consumers").consumers,
+        },
+        icons = {
+            running_animated = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+        },
+    })
 end
