bfea1f48044db18c3f0385958aa64fdb082f21b2
Gustavo Santos <gustavofsantos@outlook.com>
2023-09-26 21:03:46 -0300

    refactored code settings

--- .config/nvim/after/plugin/code.rc.lua
+++ .config/nvim/after/plugin/code.rc.lua
@@ -14,105 +14,94 @@ if has_mini_splitjoin then
 end
 
 local has_treesitter, treesitter = pcall(require, "nvim-treesitter.configs")
-if not has_treesitter then
-    return
-end
-
-treesitter.setup({
-    -- A list of parser names, or "all"
-    ensure_installed = {
-        "gitcommit",
-        "git_rebase",
-        "bash",
-        "go",
-        "lua",
-        "json",
-        "proto",
-        "toml",
-        "rust",
-        "haskell",
-        "markdown",
-        "javascript",
-        "jsdoc",
-        "typescript",
-        "tsx",
-        "prisma",
-        "python",
-        "vim",
-        "elixir",
-        "css",
-        "eex",
-        "heex",
-        "svelte",
-    },
-    highlight = {
-        enable = true,
-        use_languagetree = true,
-        additional_vim_regex_highlighting = { "markdown" },
-    },
-    indent = {
-        enable = true,
-    },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "<c-space>",
-            node_incremental = "<c-space>",
-            scope_incremental = "<c-s>",
-            node_decremental = "<c-backspace>",
+if has_treesitter then
+    treesitter.setup({
+        -- A list of parser names, or "all"
+        ensure_installed = {
+            "gitcommit",
+            "git_rebase",
+            "bash",
+            "go",
+            "lua",
+            "json",
+            "proto",
+            "toml",
+            "rust",
+            "haskell",
+            "markdown",
+            "javascript",
+            "jsdoc",
+            "typescript",
+            "tsx",
+            "prisma",
+            "python",
+            "vim",
+            "elixir",
+            "css",
+            "eex",
+            "heex",
+            "svelte",
         },
-    },
-    textobjects = {
-        select = {
+        highlight = {
             enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            use_languagetree = true,
+            additional_vim_regex_highlighting = { "markdown" },
+        },
+        indent = {
+            enable = true,
+        },
+        incremental_selection = {
+            enable = true,
             keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ["aa"] = "@parameter.outer",
-                ["ia"] = "@parameter.inner",
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
+                init_selection = "<c-space>",
+                node_incremental = "<c-space>",
+                scope_incremental = "<c-s>",
+                node_decremental = "<c-backspace>",
             },
         },
-        move = {
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                keymaps = {
+                    -- You can use the capture groups defined in textobjects.scm
+                    ["aa"] = "@parameter.outer",
+                    ["ia"] = "@parameter.inner",
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                },
+            },
+            move = {
+                enable = true,
+                set_jumps = true, -- whether to set jumps in the jumplist
+                goto_next_start = {
+                    ["]m"] = "@function.outer",
+                    ["]]"] = "@class.outer",
+                },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]["] = "@class.outer",
+                },
+                goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                    ["[["] = "@class.outer",
+                },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[]"] = "@class.outer",
+                },
+            },
+        },
+        context_commentstring = {
             enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-                ["]m"] = "@function.outer",
-                ["]]"] = "@class.outer",
-            },
-            goto_next_end = {
-                ["]M"] = "@function.outer",
-                ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-                ["[m"] = "@function.outer",
-                ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-                ["[M"] = "@function.outer",
-                ["[]"] = "@class.outer",
-            },
         },
-        swap = {
-            enable = true,
-            swap_next = {
-                ["<leader>a"] = "@parameter.inner",
-            },
-            swap_previous = {
-                ["<leader>A"] = "@parameter.inner",
-            },
-        },
-    },
-    context_commentstring = {
-        enable = true,
-    },
-})
+    })
 
--- require("nvim-treesitter.configs").setup({
---     autotag = {
---         enable = true,
---     },
--- })
+    require("nvim-treesitter.configs").setup({
+        autotag = {
+            enable = true,
+        },
+    })
+end
