6b89a4f75a3711e6f4ac60fc0eb254031cf4dac9
Gustavo Santos <gustavo.fernandes@loggi.com>
2023-10-05 15:44:03 -0300

    move config

--- .config/nvim/lua/extra/plugins/ui.lua
+++ .config/nvim/lua/extra/plugins/ui.lua
@@ -14,4 +14,38 @@ return {
     event = "LspAttach",
     opts = {},
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local lualine = require("lualine")
+      local lazy_status = require("lazy.status")
+
+      -- configure lualine with modified theme
+      lualine.setup({
+        options = { section_separators = "", component_separators = "" },
+        sections = {
+          lualine_a = {},
+          lualine_b = { "branch", "diagnostics" },
+          lualine_c = { "filename" },
+          lualine_x = {
+            {
+              lazy_status.updates,
+              cond = lazy_status.has_updates,
+            },
+          },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "filename" },
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {},
+        },
+      })
+    end,
+  },
 }
