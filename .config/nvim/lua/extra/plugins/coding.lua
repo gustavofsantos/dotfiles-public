e676c143d4901af6e93e6c283009699d91fccff3
Gustavo Santos <gustavo.fernandes@loggi.com>
2023-10-30 20:38:32 -0300

    update settings

--- .config/nvim/lua/extra/plugins/coding.lua
+++ .config/nvim/lua/extra/plugins/coding.lua
@@ -14,6 +14,26 @@ return {
     },
   },
   {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("refactoring").setup()
+    end,
+    keys = {
+      {
+        "<leader>rr",
+        function()
+          require("refactoring").select_refactor()
+        end,
+        mode = { "n", "x" },
+        desc = "Refactor",
+      },
+    },
+  },
+  {
     "folke/flash.nvim",
     event = "VeryLazy",
     opts = {},
--- .config/nvim/lua/extra/plugins/ui.lua
+++ .config/nvim/lua/extra/plugins/ui.lua
@@ -9,79 +9,13 @@ return {
     },
   },
   {
+    "yorickpeterse/nvim-pqf",
+    config = true,
+  },
+  {
     "j-hui/fidget.nvim",
     tag = "legacy",
     event = "LspAttach",
     opts = {},
-  },
-  {
-    "akinsho/bufferline.nvim",
-    enabled = false,
-    version = "*",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      local bufferline = require("bufferline")
-      bufferline.setup({
-        options = {
-          mode = "tabs",
-          themable = true,
-          separator_style = "thin",
-          always_show_bufferline = false,
-          show_buffer_icons = false,
-          show_buffer_close_icons = false,
-          show_close_icon = false,
-          indicator = {
-            style = "none",
-          },
-        },
-        highlights = {},
-      })
-
-      vim.keymap.set(
-        "n",
-        "<Tab>",
-        "<cmd>BufferLineCycleNext<cr>",
-        { noremap = true, silent = true, desc = "Cycle next tab" }
-      )
-    end,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    enabled = false,
-    config = function()
-      local lualine = require("lualine")
-      local lazy_status = require("lazy.status")
-
-      -- configure lualine with modified theme
-      lualine.setup({
-        options = {
-          section_separators = "",
-          component_separators = "",
-        },
-        sections = {
-          lualine_a = {},
-          lualine_b = { "diagnostics" },
-          lualine_c = { { "filename", file_status = true, path = 1 } },
-          lualine_x = {
-            "diff",
-            {
-              lazy_status.updates,
-              cond = lazy_status.has_updates,
-            },
-          },
-          lualine_y = {},
-          lualine_z = {},
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { { "filename", file_status = true, path = 1 } },
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = {},
-        },
-      })
-    end,
   },
 }
