1e325ef30770a4914fd47317b86317e0c9d774a4
Gustavo Santos <gustavo.fernandes@loggi.com>
2023-12-08 10:33:38 -0300

    Adds lualine config for nvim

--- .config/nvim/lua/extra/plugins/ui.lua
+++ .config/nvim/lua/extra/plugins/ui.lua
@@ -9,6 +9,28 @@ return {
     },
   },
   {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename", path = 1 },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename", path = 1 },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+    },
+  },
+  {
     "yorickpeterse/nvim-pqf",
     config = true,
   },
