4315e67d541b084d045331ef986763076758ffa6
Gustavo Santos <gustavofsantos@outlook.com>
2023-12-09 17:56:15 -0300

    update nvim ui settings

--- .config/nvim/lua/extra/plugins/ui.lua
+++ .config/nvim/lua/extra/plugins/ui.lua
@@ -15,7 +15,7 @@ return {
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename", path = 1 },
+        lualine_c = { { "filename", path = 1 } },
         lualine_x = {},
         lualine_y = {},
         lualine_z = {},
