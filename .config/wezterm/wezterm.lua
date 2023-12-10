1cece7dad7e5ec1904a7f49c316d2ca91c7fbc66
Gustavo Santos <gustavofsantos@outlook.com>
2023-12-10 12:44:32 -0300

    change wezterm settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 return {
     force_reverse_video_cursor = true,
-    color_scheme = "GruvboxDark",
+    color_scheme = "Kanagawa",
 
     font = wezterm.font("MonoLisa Nerd Font"),
     font_size = 10.0,
