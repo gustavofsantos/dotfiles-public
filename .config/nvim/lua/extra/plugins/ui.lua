68012f2c5edef59a46882209ebde9f93330c457c
Gustavo Santos <gustavofsantos@outlook.com>
2023-12-10 21:38:16 -0300

    update ui settings

--- .config/nvim/lua/extra/plugins/ui.lua
+++ .config/nvim/lua/extra/plugins/ui.lua
@@ -23,7 +23,7 @@ return {
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { "filename", path = 1 },
+        lualine_c = { { "filename", path = 1 } },
         lualine_x = { "location" },
         lualine_y = {},
         lualine_z = {},
@@ -33,5 +33,20 @@ return {
   {
     "yorickpeterse/nvim-pqf",
     config = true,
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      key_labels = {
+        ["<space>"] = "SPC",
+        ["<cr>"] = "RET",
+        ["<tab>"] = "TAB",
+      },
+    },
   },
 }
