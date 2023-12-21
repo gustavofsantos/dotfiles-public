383fb7246fd1de1ab4fbada7ed45bd446ac2a746
Gustavo Santos <gustavo.fernandes@loggi.com>
2023-12-21 11:21:53 -0300

    chore(neovim): remove commented block

--- .config/nvim/lua/extra/plugins/completion.lua
+++ .config/nvim/lua/extra/plugins/completion.lua
@@ -31,10 +31,6 @@ return {
             vim.fn["vsnip#anonymous"](args.body)
           end,
         },
-        -- window = {
-        --   completion = cmp.config.window.bordered(),
-        --   documentation = cmp.config.window.bordered(),
-        -- },
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
