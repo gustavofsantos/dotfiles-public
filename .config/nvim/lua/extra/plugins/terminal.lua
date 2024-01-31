b6c724dd8df285658cf53332e167b07cb75d861b
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-01-31 17:41:36 -0300

    feat(nvim): increase the terminal size

--- .config/nvim/lua/extra/plugins/terminal.lua
+++ .config/nvim/lua/extra/plugins/terminal.lua
@@ -6,8 +6,8 @@ return {
     terminal_mappings = true,
     direction = "horizontal",
     shade_terminals = true,
-    shading_factor = vim.o.background == "dark" and -10 or -1,
-    size = 24,
+    shading_factor = vim.o.background == "dark" and 0 or 0,
+    size = 38,
     winbar = {
       enabled = true,
       name_formatter = function(term) --  term: Terminal
