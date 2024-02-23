b8694495b59e07f3d5e8fd2226355903705a34fc
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-02-23 11:33:38 -0300

    feat(nvim): set cut key for cutless

--- .config/nvim/lua/extra/plugins/coding.lua
+++ .config/nvim/lua/extra/plugins/coding.lua
@@ -49,9 +49,7 @@ return {
   {
     "gbprod/cutlass.nvim",
     opts = {
-      -- your configuration comes here
-      -- or don't set opts to use the default settings
-      -- refer to the configuration section below
+      cut_key = "x",
     },
   },
   {
