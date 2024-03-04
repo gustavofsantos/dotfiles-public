0e0d458b3cbef9cc973421814fbd34352859a88c
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-03-04 15:20:11 -0300

    feat(nvim): update testing strategy to VTR

--- .config/nvim/lua/extra/plugins/testing.lua
+++ .config/nvim/lua/extra/plugins/testing.lua
@@ -6,7 +6,7 @@ return {
       "christoomey/vim-tmux-runner",
     },
     config = function()
-      vim.cmd([[let test#strategy = "toggleterm"]])
+      vim.cmd([[let test#strategy = "vtr"]])
       vim.cmd([[let test#javascript#playwright#options = "--headed --retries 0 --workers 1"]])
     end,
     keys = {
@@ -42,64 +42,4 @@ return {
       },
     },
   },
-  "nvim-neotest/neotest",
-  enabled = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-neotest/neotest-jest",
-  },
-  keys = {
-    {
-      "<leader>tf",
-      function()
-        require("neotest").run.run(vim.fn.expand("%"))
-      end,
-      mode = "n",
-      desc = "Test file",
-    },
-    {
-      "<leader>tn",
-      function()
-        require("neotest").run.run()
-      end,
-      mode = "n",
-      desc = "Test nearest",
-    },
-    {
-      "<leader>tw",
-      function()
-        require("neotest").run.run({ jestCommand = "jest --watch " })
-      end,
-      mode = "n",
-      desc = "Watch nearest",
-    },
-    {
-      "<leader>td",
-      function()
-        require("neotest").run.run({ strategy = "dap" })
-      end,
-      mode = "n",
-      desc = "Debug nearest test",
-    },
-  },
-  config = function()
-    require("neotest").setup({
-      discovery = {
-        enabled = false,
-      },
-      adapters = {
-        require("neotest-jest")({
-          -- jestCommand = "npm test --",
-          -- jestConfigFile = "custom.jest.config.ts",
-          jest_test_discovery = false,
-          env = { CI = true },
-          cwd = function(path)
-            return vim.fn.getcwd()
-          end,
-        }),
-      },
-    })
-  end,
 }
