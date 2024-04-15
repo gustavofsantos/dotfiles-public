a17f99ee867c4114591fefe83cf8f8d6ed2b2238
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-04-15 10:12:26 -0300

    feat(wezterm): disable configs to work with tmux

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -42,14 +42,14 @@ return {
     freetype_load_flags = "NO_HINTING",
     freetype_load_target = "Normal",
     -- front_end = "OpenGL",
-    line_height = 1.4,
+    line_height = 1.2,
     harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
     bold_brightens_ansi_colors = false,
     warn_about_missing_glyphs = false,
 
     window_decorations = "RESIZE",
 
-    window_close_confirmation = 'NeverPrompt',
+    window_close_confirmation = "NeverPrompt",
 
     window_padding = {
         left = 0,
@@ -60,163 +60,163 @@ return {
 
     use_fancy_tab_bar = false,
     tab_bar_at_bottom = true,
-    hide_tab_bar_if_only_one_tab = false,
+    hide_tab_bar_if_only_one_tab = true,
     tab_max_width = 999999,
 
-    leader = { key = "a", mods = "CTRL" },
-
-    keys = {
-        {
-            key = "s",
-            mods = "LEADER",
-            action = wezterm.action({
-                SplitHorizontal = { domain = "CurrentPaneDomain" },
-            }),
-        },
-        {
-            key = "S",
-            mods = "LEADER",
-            action = wezterm.action({
-                SplitVertical = { domain = "CurrentPaneDomain" },
-            }),
-        },
-        {
-            key = "c",
-            mods = "LEADER",
-            action = wezterm.action({
-                SpawnTab = "CurrentPaneDomain",
-            }),
-        },
-        {
-            key = "h",
-            mods = "LEADER",
-            action = wezterm.action({
-                ActivatePaneDirection = "Left",
-            }),
-        },
-        {
-            key = "j",
-            mods = "LEADER",
-            action = wezterm.action({
-                ActivatePaneDirection = "Down",
-            }),
-        },
-        {
-            key = "k",
-            mods = "LEADER",
-            action = wezterm.action({
-                ActivatePaneDirection = "Up",
-            }),
-        },
-        {
-            key = "l",
-            mods = "LEADER",
-            action = wezterm.action({
-                ActivatePaneDirection = "Right",
-            }),
-        },
-        {
-            key = "1",
-            mods = "LEADER",
-            action = wezterm.action({ ActivateTab = 0 }),
-        },
-        {
-            key = "2",
-            mods = "LEADER",
-            action = wezterm.action({ ActivateTab = 1 }),
-        },
-        {
-            key = "3",
-            mods = "LEADER",
-            action = wezterm.action({ ActivateTab = 2 }),
-        },
-        {
-            key = "4",
-            mods = "LEADER",
-            action = wezterm.action({ ActivateTab = 3 }),
-        },
-        {
-            key = "5",
-            mods = "LEADER",
-            action = wezterm.action({ ActivateTab = 4 }),
-        },
-        {
-            key = "6",
-            mods = "LEADER",
-            action = wezterm.action({ ActivateTab = 5 }),
-        },
-        {
-            key = "7",
-            mods = "LEADER",
-            action = wezterm.action({ ActivateTab = 6 }),
-        },
-        {
-            key = "8",
-            mods = "LEADER",
-            action = wezterm.action({ ActivateTab = 7 }),
-        },
-        {
-            key = "9",
-            mods = "LEADER",
-            action = wezterm.action({ ActivateTab = 8 }),
-        },
-        --   {
-        --     key = "Tab",
-        --     mods = "LEADER",
-        --     action = wezterm.action { ActivateTabRelative = 1 }
-        --   },
-        --   {
-        --     key = "w",
-        --     mods = "LEADER",
-        --     action = wezterm.action.ShowTabNavigator,
-        --   },
-        --   {
-        --     key = "e",
-        --     mods = "LEADER",
-        --     action = wezterm.action.PromptInputLine {
-        --       description = "New tab name",
-        --       action = wezterm.action_callback(function(window, pane, line)
-        --         if line then
-        --           window:active_tab():set_title(line)
-        --         end
-        --       end)
-        --     }
-        --   },
-        {
-            key = "o",
-            mods = "LEADER",
-            action = wezterm.action.RotatePanes("Clockwise"),
-        },
-        --   {
-        --     key = "n",
-        --     mods = "LEADER",
-        --     action = wezterm.action.ToggleFullScreen,
-        --   },
-        {
-            key = "z",
-            mods = "LEADER",
-            action = wezterm.action.TogglePaneZoomState,
-        },
-        --   {
-        --     key = 'l',
-        --     mods = 'LEADER',
-        --     action = wezterm.action.ShowLauncherArgs {
-        --       flags = 'FUZZY|WORKSPACES',
-        --     },
-        {
-            key = ",",
-            mods = "LEADER",
-            action = wezterm.action.PromptInputLine({
-                description = "Enter new name for tab",
-                action = wezterm.action_callback(function(window, pane, line)
-                    -- line will be `nil` if they hit escape without entering anything
-                    -- An empty string if they just hit enter
-                    -- Or the actual line of text they wrote
-                    if line then
-                        window:active_tab():set_title(line)
-                    end
-                end),
-            }),
-        },
-    },
+    -- leader = { key = "a", mods = "CTRL" },
+    --
+    -- keys = {
+    --     {
+    --         key = "s",
+    --         mods = "LEADER",
+    --         action = wezterm.action({
+    --             SplitHorizontal = { domain = "CurrentPaneDomain" },
+    --         }),
+    --     },
+    --     {
+    --         key = "S",
+    --         mods = "LEADER",
+    --         action = wezterm.action({
+    --             SplitVertical = { domain = "CurrentPaneDomain" },
+    --         }),
+    --     },
+    --     {
+    --         key = "c",
+    --         mods = "LEADER",
+    --         action = wezterm.action({
+    --             SpawnTab = "CurrentPaneDomain",
+    --         }),
+    --     },
+    --     {
+    --         key = "h",
+    --         mods = "LEADER",
+    --         action = wezterm.action({
+    --             ActivatePaneDirection = "Left",
+    --         }),
+    --     },
+    --     {
+    --         key = "j",
+    --         mods = "LEADER",
+    --         action = wezterm.action({
+    --             ActivatePaneDirection = "Down",
+    --         }),
+    --     },
+    --     {
+    --         key = "k",
+    --         mods = "LEADER",
+    --         action = wezterm.action({
+    --             ActivatePaneDirection = "Up",
+    --         }),
+    --     },
+    --     {
+    --         key = "l",
+    --         mods = "LEADER",
+    --         action = wezterm.action({
+    --             ActivatePaneDirection = "Right",
+    --         }),
+    --     },
+    --     {
+    --         key = "1",
+    --         mods = "LEADER",
+    --         action = wezterm.action({ ActivateTab = 0 }),
+    --     },
+    --     {
+    --         key = "2",
+    --         mods = "LEADER",
+    --         action = wezterm.action({ ActivateTab = 1 }),
+    --     },
+    --     {
+    --         key = "3",
+    --         mods = "LEADER",
+    --         action = wezterm.action({ ActivateTab = 2 }),
+    --     },
+    --     {
+    --         key = "4",
+    --         mods = "LEADER",
+    --         action = wezterm.action({ ActivateTab = 3 }),
+    --     },
+    --     {
+    --         key = "5",
+    --         mods = "LEADER",
+    --         action = wezterm.action({ ActivateTab = 4 }),
+    --     },
+    --     {
+    --         key = "6",
+    --         mods = "LEADER",
+    --         action = wezterm.action({ ActivateTab = 5 }),
+    --     },
+    --     {
+    --         key = "7",
+    --         mods = "LEADER",
+    --         action = wezterm.action({ ActivateTab = 6 }),
+    --     },
+    --     {
+    --         key = "8",
+    --         mods = "LEADER",
+    --         action = wezterm.action({ ActivateTab = 7 }),
+    --     },
+    --     {
+    --         key = "9",
+    --         mods = "LEADER",
+    --         action = wezterm.action({ ActivateTab = 8 }),
+    --     },
+    --   {
+    --     key = "Tab",
+    --     mods = "LEADER",
+    --     action = wezterm.action { ActivateTabRelative = 1 }
+    --   },
+    --   {
+    --     key = "w",
+    --     mods = "LEADER",
+    --     action = wezterm.action.ShowTabNavigator,
+    --   },
+    --   {
+    --     key = "e",
+    --     mods = "LEADER",
+    --     action = wezterm.action.PromptInputLine {
+    --       description = "New tab name",
+    --       action = wezterm.action_callback(function(window, pane, line)
+    --         if line then
+    --           window:active_tab():set_title(line)
+    --         end
+    --       end)
+    --     }
+    --   },
+    -- {
+    --     key = "o",
+    --     mods = "LEADER",
+    --     action = wezterm.action.RotatePanes("Clockwise"),
+    -- },
+    --   {
+    --     key = "n",
+    --     mods = "LEADER",
+    --     action = wezterm.action.ToggleFullScreen,
+    --   },
+    -- {
+    --     key = "z",
+    --     mods = "LEADER",
+    --     action = wezterm.action.TogglePaneZoomState,
+    -- },
+    --   {
+    --     key = 'l',
+    --     mods = 'LEADER',
+    --     action = wezterm.action.ShowLauncherArgs {
+    --       flags = 'FUZZY|WORKSPACES',
+    --     },
+    -- {
+    --     key = ",",
+    --     mods = "LEADER",
+    --     action = wezterm.action.PromptInputLine({
+    --         description = "Enter new name for tab",
+    --         action = wezterm.action_callback(function(window, pane, line)
+    --             -- line will be `nil` if they hit escape without entering anything
+    --             -- An empty string if they just hit enter
+    --             -- Or the actual line of text they wrote
+    --             if line then
+    --                 window:active_tab():set_title(line)
+    --             end
+    --         end),
+    --     }),
+    -- },
+    -- },
 }
