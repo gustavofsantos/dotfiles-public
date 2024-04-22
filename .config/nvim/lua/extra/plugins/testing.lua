306aaa49d3f50ddaafebc625c621b3588465d096
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-04-22 10:13:27 -0300

    refactor(nvim): remove duplication

--- .config/nvim/lua/extra/plugins/testing.lua
+++ .config/nvim/lua/extra/plugins/testing.lua
@@ -10,11 +10,12 @@ return {
 
       vim.cmd("autocmd VimEnter * lua local ok, err = pcall(vim.cmd, 'VtrAttachToPane 1')")
 
-      vim.keymap.set("n", "<leader>tn", "<cmd>TestNearest<cr>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<cr>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<leader>ta", "<cmd>TestSuite<cr>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<cr>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<leader>tv", "<cmd>TestVisit<cr>", { noremap = true, silent = true })
+      local opts = { noremap = true, silent = true }
+      vim.keymap.set("n", "<leader>tn", "<cmd>TestNearest<cr>", opts)
+      vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<cr>", opts)
+      vim.keymap.set("n", "<leader>ta", "<cmd>TestSuite<cr>", opts)
+      vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<cr>", opts)
+      vim.keymap.set("n", "<leader>tv", "<cmd>TestVisit<cr>", opts)
     end,
   },
 }
