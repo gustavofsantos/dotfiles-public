fe58f9225ea2ac00145df00d895a5e7444369753
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-06-04 11:14:12 -0300

    feat(nvim): setup task runner for linters

--- .config/nvim/lua/extra/plugins/task_runner.lua
+++ .config/nvim/lua/extra/plugins/task_runner.lua
@@ -1,6 +1,57 @@
 return {
   "stevearc/overseer.nvim",
   config = function()
-    require("overseer").setup()
+    local overseer = require("overseer")
+    overseer.setup()
+
+    -- beyond-py
+    overseer.register_template({
+      name = "beyond - run flake8",
+      builder = function(params)
+        return {
+          cmd = { "poetry", "run", "flake8" },
+          args = {},
+          cwd = "/opt/loggi/py/apps/beyond/",
+          components = { "default" },
+          env = {},
+        }
+      end,
+      condition = {
+        filetype = "python",
+        dir = "/opt/loggi/py/apps/beyond/",
+      },
+    })
+
+    overseer.register_template({
+      name = "beyond - run black",
+      builder = function(params)
+        return {
+          cmd = { "poetry", "run", "black" },
+          args = { "--check", "." },
+          cwd = "/opt/loggi/py/apps/beyond/",
+          components = { "default" },
+          env = {},
+        }
+      end,
+      condition = {
+        filetype = "python",
+        dir = "/opt/loggi/py/apps/beyond/",
+      },
+    })
+
+    vim.api.nvim_create_user_command("OverseerRestartLast", function()
+      local overseer = require("overseer")
+      local tasks = overseer.list_tasks({ recent_first = true })
+      if vim.tbl_isempty(tasks) then
+        vim.notify("No tasks found", vim.log.levels.WARN)
+      else
+        overseer.run_action(tasks[1], "restart")
+      end
+    end, {})
   end,
+  keys = {
+    { "<leader>rr", "<cmd>OverseerRun<cr>", { desc = "Run", noremap = true, silent = true } },
+    { "<leader>rl", "<cmd>OverseerRestartLast<cr>", { desc = "Run last", noremap = true, silent = true } },
+    { "<leader>rt", "<cmd>OverseerToggle<cr>", { desc = "Toggle", noremap = true, silent = true } },
+  },
 }
