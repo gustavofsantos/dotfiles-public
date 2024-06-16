430ef80aeb44234cd19a2fe1e9739fe129f91090
Gustavo Santos <gustavogustav54@gmail.com>
2024-06-16 11:06:40 -0300

    chore(wezterm): change theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -34,7 +34,7 @@ end)
 return {
     automatically_reload_config = false,
     force_reverse_video_cursor = true,
-    color_scheme = "Kanagawa",
+    color_scheme = "Modus Operandi",
 
     font = wezterm.font("MonoLisa Nerd Font"),
     -- font_size = 10.0,
