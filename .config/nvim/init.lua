7483e75c7a03f406facf7f385322ea2aeb57722d
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-06-18 09:58:44 -0300

    feat(nvim): delete plugins to disable

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,29 +2,6 @@ require("settings")
 require("keymappings")
 require("notes")
 
-local disabled_built_ins = {
-  "2html_plugin",
-  "getscript",
-  "getscriptPlugin",
-  "gzip",
-  "logipat",
-  -- "netrw",
-  -- "netrwPlugin",
-  -- "netrwSettings",
-  -- "netrwFileHandlers",
-  "tar",
-  "tarPlugin",
-  "rrhelper",
-  "vimball",
-  "vimballPlugin",
-  "zip",
-  "zipPlugin",
-}
-
-for _, plugin in pairs(disabled_built_ins) do
-  vim.g["loaded_" .. plugin] = 1
-end
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
