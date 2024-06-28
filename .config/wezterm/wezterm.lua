d7bdf5d9b73541b1028b52cfe4ebe6ca350710e3
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-06-28 17:36:38 -0300

    feat(wezterm): add warp light theme variant

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -34,7 +34,7 @@ end)
 return {
     automatically_reload_config = false,
     force_reverse_video_cursor = true,
-    color_scheme = "Kanagawa",
+    color_scheme = "warp_dark",
 
     font = wezterm.font("MonoLisa Nerd Font"),
     -- font = wezterm.font("OperatorMono Nerd Font", { weight = "Medium", }),
