611b16f100c4dfd05b352dc0fbabbd1ad2ae01fe
Gustavo Santos <gustavogustav54@gmail.com>
2024-07-09 16:34:59 -0300

    feat(nvim): better launch control

--- .config/nvim/lua/launch_control.lua
+++ .config/nvim/lua/launch_control.lua
@@ -1,175 +1,118 @@
-local actions = require "telescope.actions"
-local action_state = require "telescope.actions.state"
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local themes = require("telescope.themes")
 local conf = require("telescope.config").values
+local log = require("plenary.log").new({})
+log.level = "debug"
 
-
-local icons = {
-  file = " ",
+local categories = {
+  action = "",
+  navigation = "",
   vcs = "",
   lsp = "󰀫",
-  problem = " "
+  problem = "",
 }
 
-local mappings = {}
-mappings.navigation = {
-  { "Find files",        cmd = "Telescope find_files",            tag = "file" },
-  { "Recent files",      cmd = "Telescope oldfiles",              tag = "file" },
-  { "Open files",        cmd = "Telescope buffers",               tag = "file" },
-  { "Commits",           cmd = "Telescope git_commits",           tag = "vcs" },
-  { "Branches",          cmd = "Telescope git_branches",          tag = "vcs" },
-  { "Symbols",           cmd = "Telescope lsp_document_symbols",  tag = "lsp" },
-  { "Workspace symbols", cmd = "Telescope lsp_workspace_symbols", tag = "lsp" },
-}
+local mappings = {
+  { "Find files", cmd = "Telescope find_files", category = "navigation" },
+  { "Recent files", cmd = "Telescope oldfiles", category = "navigation" },
+  { "Open files", cmd = "Telescope buffers", category = "navigation" },
+  { "Recent locations", cmd = "Telescope jumplist theme=dropdown", category = "navigation" },
+  { "Text search", cmd = "Telescope current_buffer_fuzzy_find theme=dropdown", category = "navigation" },
+  { "Workspace text search", cmd = "Telescope live_grep", category = "navigation" },
+  { "Commits", cmd = "Telescope git_commits", category = "navigation" },
+  { "Branches", cmd = "Telescope git_branches", category = "navigation" },
+  { "Symbols", cmd = "Telescope lsp_document_symbols", category = "navigation" },
+  { "Classes", cmd = "Telescope lsp_document_symbols symbols=class", category = "navigation" },
+  { "Functions", cmd = "Telescope lsp_document_symbols symbols=function", category = "navigation" },
+  { "Variables", cmd = "Telescope lsp_document_symbols symbols=variable", category = "navigation" },
+  { "Workspace symbols", cmd = "Telescope lsp_workspace_symbols", category = "navigation" },
 
-mappings.vcs = {
-  { "Commit",       cmd = "Git commit",            tag = "vcs" },
-  { "Pull",         cmd = "Git pull",              tag = "vcs" },
-  { "Push",         cmd = "Git push",              tag = "vcs" },
-  { "Blame",        cmd = "Git blame",             tag = "vcs" },
-  { "Stage hunk",   cmd = "Gitsigns stage_hunk",   tag = "vcs" },
-  { "Stage buffer", cmd = "Git add %",             tag = "vcs" },
-  { "Reset hunk",   cmd = "Gitsigns reset_hunk",   tag = "vcs" },
-  { "Reset buffer", cmd = "Gitsigns reset_buffer", tag = "vcs" },
-  { "Preview hunk", cmd = "Gitsigns preview_hunk", tag = "vcs" },
-}
+  -- version control actions
+  { "Commit", cmd = "Git commit", category = "vcs" },
+  { "Pull", cmd = "Git pull", category = "vcs" },
+  { "Push", cmd = "Git push", category = "vcs" },
+  { "Blame", cmd = "Git blame", category = "vcs" },
+  { "Stage hunk", cmd = "Gitsigns stage_hunk", category = "vcs" },
+  { "Stage buffer", cmd = "Git add %", category = "vcs" },
+  { "Reset hunk", cmd = "Gitsigns reset_hunk", category = "vcs" },
+  { "Reset buffer", cmd = "Gitsigns reset_buffer", category = "vcs" },
+  { "Preview hunk", cmd = "Gitsigns preview_hunk", category = "vcs" },
 
-mappings.problems = {
-  { "Buffer diagnostics",    cmd = "Telescope diagnostics bufnr=0", tag = "problem" },
-  { "Workspace diagnostics", cmd = "Telescope diagnostics",         tag = "problem" },
+  -- problem actions
+  { "Buffer diagnostics", cmd = "Telescope diagnostics bufnr=0", category = "problem" },
+  { "Workspace diagnostics", cmd = "Telescope diagnostics", category = "problem" },
+
+  -- basic actions
+  { "Split window", cmd = "vsplit", category = "action" },
+  { "Split window below", cmd = "split", category = "action" },
+  { "Disable formatting", cmd = "FormatDisable", category = "action" },
+  { "Enable formatting", cmd = "FormatEnable", category = "action" },
+  { "Run", cmd = "OverseerRun", category = "action" },
+  { "Runs", cmd = "OverseerToggle", category = "action" },
+  { "New tab", cmd = "tabnew", category = "action" },
+  { "Run file", cmd = "!%", category = "action" },
+  { "Close buffer", cmd = "bd", category = "action" },
+  { "Quit", cmd = "q", category = "action" },
 }
 
 local dropdown_theme = themes.get_dropdown({})
 
---@param cmd string
+---@param cmd string
 local execute_cmd = function(cmd)
   vim.cmd(cmd)
 end
 
-local create_submenu_picker = function(opts, cfg)
-  opts = opts or {}
-  print("create submenu picker" .. vim.inspect(cfg.mappings))
-  return pickers.new(dropdown_theme, {
-    prompt_title = cfg.prompt_title or "Launch Control",
-    sorter = conf.generic_sorter(opts),
-    finder = finders.new_table({
-      results = cfg.mappings,
-      entry_maker = function(entry)
-        return {
-          value = entry,
-          display = entry[1],
-          ordinal = entry[1],
-          cmd = entry.cmd
-        }
-      end
-    }),
-    attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local selection = action_state.get_selected_entry()
-        execute_cmd(selection.cmd)
-      end)
-
-      return true
-    end
-  }):find()
+---@param category string
+local get_category_icon = function(category)
+  return categories[category] or ""
 end
-
-local new_options = function()
-  return {
-    {
-      "Find",
-      exec = function(opts)
-        create_submenu_picker(opts,
-          { prompt_title = "Find", mappings = mappings.navigation })
-      end
-    },
-    {
-      "VCS",
-      exec = function(opts)
-        create_submenu_picker(opts, { prompt_title = "VCS", mappings = mappings.vcs })
-      end
-    },
-    {
-      "Problems",
-      exec = function(opts)
-        create_submenu_picker(opts, { prompt_title = "Problems", mappings = mappings.problems })
-      end
-    },
-  }
-end
-
-
-local new_main_picker = function(opts)
-  local categories = new_options()
-  pickers.new(dropdown_theme, {
-    prompt_title = "Launch Control",
-    prompt_prefix = " ",
-    sorter = conf.generic_sorter(opts),
-    finder = finders.new_table({
-      results = categories,
-      entry_maker = function(option)
-        return {
-          value = option,
-          display = option[1],
-          ordinal = option[1],
-          exec = option.exec
-        }
-      end
-    }),
-    attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local selection = action_state.get_selected_entry()
-        selection.exec(opts)
-      end)
-
-      return true
-    end
-  }):find()
-end
-
 
 local launch_control = function()
-  print("Run launch control")
-  new_main_picker()
-end
-
-local launch_control_all = function()
   local opts = {}
 
-  local all_options = {}
-  all_options = vim.list_extend(all_options, mappings.navigation)
-  all_options = vim.list_extend(all_options, mappings.vcs)
-  all_options = vim.list_extend(all_options, mappings.problems)
+  for _, option in ipairs(mappings) do
+    if option.nkeymap then
+      local keymap = option.nkeymap
+      local cmd = option.cmd
 
-  pickers.new(dropdown_theme, {
-    prompt_title = "Launch Control",
-    prompt_prefix = "🚀 ",
-    sorter = conf.generic_sorter(opts),
-    finder = finders.new_table({
-      results = all_options,
-      entry_maker = function(option)
-        return {
-          value = option,
-          display = icons[option.tag] .. " " .. option[1],
-          ordinal = option[1],
-          cmd = option.cmd
-        }
-      end
-    }),
-    attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local selection = action_state.get_selected_entry()
-        execute_cmd(selection.cmd)
-      end)
-
-      return true
+      vim.api.nvim_set_keymap("n", keymap, "<cmd> " .. cmd .. "<CR>", { noremap = true, silent = true })
     end
-  }):find()
+  end
+
+  pickers
+    .new(dropdown_theme, {
+      prompt_title = "Launch Control",
+      prompt_prefix = " ",
+      sorter = conf.generic_sorter(opts),
+      finder = finders.new_table({
+        results = mappings,
+        entry_maker = function(option)
+          return {
+            value = option,
+            display = get_category_icon(option.category) .. " " .. option[1],
+            ordinal = option[1],
+            cmd = option.cmd,
+          }
+        end,
+      }),
+      attach_mappings = function(prompt_bufnr, map)
+        actions.select_default:replace(function()
+          actions.close(prompt_bufnr)
+          local selection = action_state.get_selected_entry()
+          if type(selection.cmd) == "function" then
+            selection.cmd(opts)
+          else
+            execute_cmd(selection.cmd)
+          end
+        end)
+
+        return true
+      end,
+    })
+    :find()
 end
 
-vim.api.nvim_create_user_command("LaunchControl", launch_control_all, {})
+vim.api.nvim_create_user_command("LaunchControl", launch_control, {})
