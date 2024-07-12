e186b7248f1757f4d188199b34bdb498cdb944e5
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-07-12 09:28:48 -0300

    feat(nvim): add tasks to improve development speed

--- .config/nvim/lua/extra/plugins/task_runner.lua
+++ .config/nvim/lua/extra/plugins/task_runner.lua
@@ -1,73 +1,203 @@
+local register_payment_tasks = function(overseer)
+  overseer.register_template({
+    name = "payment - start dev server",
+    builder = function()
+      return {
+        cmd = { "poetry@payment-api" },
+        args = { "run", "uvicorn", "app.main:app", "--host", "0.0.0.0",
+          "--port", "8000", "--workers", "1", "--no-access-log", "--reload" },
+        cwd = "/opt/loggi/beyond-payment/",
+        components = { "default" },
+        env = {},
+      }
+    end,
+  })
+
+  overseer.register_template({
+    name = "payment - format (ruff)",
+    builder = function()
+      return {
+        cmd = { "poetry@payment-api" },
+        args = { "run", "ruff", "format", "." },
+        cwd = "/opt/loggi/beyond-payment/",
+        components = { "default" },
+        env = {},
+      }
+    end,
+  })
+
+  overseer.register_template({
+    name = "payment - lint (ruff)",
+    builder = function()
+      return {
+        cmd = { "poetry@payment-api" },
+        args = { "run", "ruff", "check", "--fix", "." },
+        cwd = "/opt/loggi/beyond-payment/",
+        components = { "default" },
+        env = {},
+      }
+    end,
+  })
+
+  overseer.register_template({
+    name = "payment - lint (mypy)",
+    builder = function()
+      return {
+        cmd = { "poetry@payment-api" },
+        args = { "run", "mypy", "." },
+        cwd = "/opt/loggi/beyond-payment/",
+        components = { "default" },
+        env = {},
+      }
+    end,
+  })
+
+  overseer.register_template({
+    name = "payment - run migrations",
+    builder = function()
+      return {
+        cmd = { "poetry@payment-api", "run", "alembic", "upgrade", "head" },
+        args = {},
+        cwd = "/opt/loggi/beyond-payment/",
+        components = { "default" },
+        env = { APP_DB_HOST = "localhost" },
+      }
+    end,
+    condition = {
+      filetype = "python",
+      dir = "/opt/loggi/beyond-payment/",
+    },
+  })
+
+  overseer.register_template({
+    name = "payment - run migrations (test)",
+    builder = function()
+      return {
+        cmd = { "poetry@payment-api", "run", "alembic", "-x", "env=test", "upgrade", "head" },
+        args = {},
+        cwd = "/opt/loggi/beyond-payment/",
+        components = { "default" },
+        env = { APP_DB_HOST = "localhost" },
+      }
+    end,
+    condition = {
+      filetype = "python",
+      dir = "/opt/loggi/beyond-payment/",
+    },
+  })
+
+  overseer.register_template({
+    name = "payment - run migrations down",
+    builder = function()
+      return {
+        cmd = { "poetry@payment-api", "run", "alembic", "downgrade", "-1" },
+        args = {},
+        cwd = "/opt/loggi/beyond-payment/",
+        components = { "default" },
+        env = { APP_DB_HOST = "localhost" },
+      }
+    end,
+    condition = {
+      filetype = "python",
+      dir = "/opt/loggi/beyond-payment/",
+    },
+  })
+
+  overseer.register_template({
+    name = "payment - run migrations down (test)",
+    builder = function()
+      return {
+        cmd = { "poetry@payment-api", "run", "alembic", "-x", "env=test", "downgrade", "-1" },
+        args = {},
+        cwd = "/opt/loggi/beyond-payment/",
+        components = { "default" },
+        env = { APP_DB_HOST = "localhost" },
+      }
+    end,
+    condition = {
+      filetype = "python",
+      dir = "/opt/loggi/beyond-payment/",
+    },
+  })
+end
+
+local register_beyond_tasks = function(overseer)
+  overseer.register_template({
+    name = "beyond - run flake8",
+    builder = function(params)
+      return {
+        cmd = { "poetry", "run", "flake8" },
+        args = {},
+        cwd = "/opt/loggi/py/apps/beyond/",
+        components = { "default" },
+        env = {},
+      }
+    end,
+    condition = {
+      filetype = "python",
+      dir = "/opt/loggi/py/apps/beyond/",
+    },
+  })
+
+  overseer.register_template({
+    name = "beyond - run black check",
+    builder = function(params)
+      return {
+        cmd = { "poetry", "run", "black" },
+        args = { "--check", "." },
+        cwd = "/opt/loggi/py/apps/beyond/",
+        components = { "default" },
+        env = {},
+      }
+    end,
+    condition = {
+      filetype = "python",
+      dir = "/opt/loggi/py/apps/beyond/",
+    },
+  })
+
+  overseer.register_template({
+    name = "beyond - run black format",
+    builder = function(params)
+      return {
+        cmd = { "poetry", "run", "black" },
+        args = { "." },
+        cwd = "/opt/loggi/py/apps/beyond/",
+        components = { "default" },
+        env = {},
+      }
+    end,
+    condition = {
+      filetype = "python",
+      dir = "/opt/loggi/py/apps/beyond/",
+    },
+  })
+
+  overseer.register_template({
+    name = "beyond - build proto",
+    builder = function()
+      return {
+        cmd = { "poetry", "run", "python" },
+        args = { "../../libs/buildproto.py", "apps/beyond/src/proto" },
+        cwd = "/opt/loggi/py/apps/beyond/",
+        components = { "default" },
+        env = {},
+      }
+    end,
+    condition = {
+      filetype = "python",
+      dir = "/opt/loggi/py/apps/beyond/",
+    },
+  })
+end
+
 return {
   "stevearc/overseer.nvim",
   config = function()
     local overseer = require("overseer")
     overseer.setup()
-
-    -- beyond-py
-    overseer.register_template({
-      name = "beyond - run flake8",
-      builder = function(params)
-        return {
-          cmd = { "poetry", "run", "flake8" },
-          args = {},
-          cwd = "/opt/loggi/py/apps/beyond/",
-          components = { "default" },
-          env = {},
-        }
-      end,
-      condition = {
-        filetype = "python",
-        dir = "/opt/loggi/py/apps/beyond/",
-      },
-    })
-
-    overseer.register_template({
-      name = "beyond - run black check",
-      builder = function(params)
-        return {
-          cmd = { "poetry", "run", "black" },
-          args = { "--check", "." },
-          cwd = "/opt/loggi/py/apps/beyond/",
-          components = { "default" },
-          env = {},
-        }
-      end,
-      condition = {
-        filetype = "python",
-        dir = "/opt/loggi/py/apps/beyond/",
-      },
-    })
-
-    overseer.register_template({
-      name = "beyond - run black format",
-      builder = function(params)
-        return {
-          cmd = { "poetry", "run", "black" },
-          args = { "." },
-          cwd = "/opt/loggi/py/apps/beyond/",
-          components = { "default" },
-          env = {},
-        }
-      end,
-      condition = {
-        filetype = "python",
-        dir = "/opt/loggi/py/apps/beyond/",
-      },
-    })
-
-    overseer.register_template({
-      name = "beyond - build proto",
-      builder = function()
-        return {
-          cmd = { "poetry", "run", "python" },
-          args = { "../../libs/buildproto.py", "apps/beyond/src/proto" },
-          cwd = "/opt/loggi/py/apps/beyond/",
-          components = { "default" },
-          env = {},
-        }
-      end,
-    })
+    register_beyond_tasks(overseer)
+    register_payment_tasks(overseer)
 
     vim.api.nvim_create_user_command("OverseerRestartLast", function()
       local overseer = require("overseer")
