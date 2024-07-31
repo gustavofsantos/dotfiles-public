f9b80b4b2b87370b68da500ba2eb2a180f9f3d03
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-07-31 11:59:44 -0300

    feat(nvim): include refactoring plugin

--- .config/nvim/lua/extra/plugins/coding.lua
+++ .config/nvim/lua/extra/plugins/coding.lua
@@ -47,7 +47,23 @@ return {
     --   { ",v", "<cmd>VenvSelect<cr>" },
     -- },
   },
-
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("refactoring").setup()
+    end,
+    keys = {
+      {
+        "<leader>R",
+        mode = { "n", "x" },
+        function() require('refactoring').select_refactor() end
+      }
+    }
+  },
   {
     "mfussenegger/nvim-dap",
     dependencies = {
