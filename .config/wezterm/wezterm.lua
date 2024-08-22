4f5e3f5bd17293ff26ba5d0997a97c5bd4304daf
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-08-22 17:43:59 -0300

    feat(wezterm): config navigation between panes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 wezterm.on("update-right-status", function(window, pane)
     local date = wezterm.strftime("%Y-%m-%d %H:%M:%S")
@@ -29,6 +30,38 @@ wezterm.on("user-var-changed", function(window, pane, name, value)
         end
     end
     window:set_config_overrides(overrides)
+end)
+
+local function isViProcess(pane)
+    -- get_foreground_process_name On Linux, macOS and Windows,
+    -- the process can be queried to determine this path. Other operating systems
+    -- (notably, FreeBSD and other unix systems) are not currently supported
+    return pane:get_foreground_process_name():find('n?vim') ~= nil or pane:get_title():find("n?vim") ~= nil
+end
+
+local function conditionalActivatePane(window, pane, pane_direction, vim_direction)
+    if isViProcess(pane) then
+        window:perform_action(
+        -- This should match the keybinds you set in Neovim.
+            act.SendKey({ key = vim_direction, mods = 'CTRL' }),
+            pane
+        )
+    else
+        window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
+    end
+end
+
+wezterm.on('ActivatePaneDirection-right', function(window, pane)
+    conditionalActivatePane(window, pane, 'Right', 'l')
+end)
+wezterm.on('ActivatePaneDirection-left', function(window, pane)
+    conditionalActivatePane(window, pane, 'Left', 'h')
+end)
+wezterm.on('ActivatePaneDirection-up', function(window, pane)
+    conditionalActivatePane(window, pane, 'Up', 'k')
+end)
+wezterm.on('ActivatePaneDirection-down', function(window, pane)
+    conditionalActivatePane(window, pane, 'Down', 'j')
 end)
 
 local config = {}
@@ -93,6 +126,10 @@ config.keys = {
             SpawnTab = "CurrentPaneDomain",
         }),
     },
+    { key = 'h', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-left') },
+    { key = 'j', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-down') },
+    { key = 'k', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-up') },
+    { key = 'l', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-right') },
     {
         key = "h",
         mods = "LEADER",
