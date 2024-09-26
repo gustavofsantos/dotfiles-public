6ce7264811f501f33b33607a7dfad644b10ccfc4
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-09-26 11:50:49 -0300

    feat(nvim): bring back incline

--- .config/nvim/lua/extra/plugins/ui.lua
+++ .config/nvim/lua/extra/plugins/ui.lua
@@ -1,5 +1,5 @@
 return {
-  {
+  { -- dressing
     "stevearc/dressing.nvim",
     dependencies = { "nvim-telescope/telescope.nvim" },
     opts = {
@@ -9,6 +9,81 @@ return {
     },
   },
   {
+    'b0o/incline.nvim',
+    config = function()
+      require('incline').setup({
+        debounce_threshold = {
+          falling = 50,
+          rising = 10
+        },
+        hide = {
+          cursorline = false,
+          focused_win = false,
+          only_win = true -- Hide incline if only one window in tab
+        },
+        highlight = {
+          groups = {
+            InclineNormal = {
+              default = true,
+              group = "CursorLineNr"
+            },
+            InclineNormalNC = {
+              default = true,
+              group = "NormalFloat"
+            }
+          }
+        },
+        ignore = {
+          buftypes = "special",
+          filetypes = {},
+          floating_wins = true,
+          unlisted_buffers = true,
+          wintypes = "special"
+        },
+        window = {
+          margin = {
+            horizontal = 1,
+            vertical = 2
+          },
+          options = {
+            signcolumn = "no",
+            wrap = false
+          },
+          padding = 1,
+          padding_char = " ",
+          placement = {
+            horizontal = "right",
+            vertical = "top"
+          },
+          width = "fit",
+          winhighlight = {
+            active = {
+              EndOfBuffer = "None",
+              Normal = "InclineNormal",
+              Search = "None"
+            },
+            inactive = {
+              EndOfBuffer = "None",
+              Normal = "InclineNormalNC",
+              Search = "None"
+            }
+          },
+          zindex = 50
+        },
+        render = function(props)
+          local fname = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+
+          return {
+            {
+              fname,
+            }
+          }
+        end,
+      })
+    end,
+    event = 'VeryLazy',
+  },
+  { -- lualine
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
@@ -97,7 +172,7 @@ return {
       })
     end,
   },
-  {
+  { -- pqf
     "yorickpeterse/nvim-pqf",
     config = true,
   },
