38ede18377801018e7750836131cb5f88b3755ee
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-10-15 09:18:42 -0300

    feat(fish): update settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -46,8 +46,8 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /home/gustavo/anaconda3/bin/conda
-    eval /home/gustavo/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+if test -f "$HOME/anaconda3/bin/conda"
+    eval "$HOME/anaconda3/bin/conda" "shell.fish" "hook" $argv | source
 end
 # <<< conda initialize <<<
 
