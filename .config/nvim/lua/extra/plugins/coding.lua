0f1db73c266b7294addaeebaab89cab9ff6c47cb
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-10-24 08:58:24 -0300

    chore(nvim): remove venv selector

--- .config/nvim/lua/extra/plugins/coding.lua
+++ .config/nvim/lua/extra/plugins/coding.lua
@@ -1,27 +1,5 @@
 return {
   {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
-      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-    },
-    lazy = false,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
-    config = function()
-      require("venv-selector").setup({
-        settings = {
-          options = {
-            debug = true
-          }
-        }
-      })
-    end,
-    -- keys = {
-    --   { ",v", "<cmd>VenvSelect<cr>" },
-    -- },
-  },
-  {
     "AndrewRadev/switch.vim",
     config = function()
       vim.cmd([[let g:switch_custom_definitions =
