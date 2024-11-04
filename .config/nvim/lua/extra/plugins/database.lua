d711339a7aa50b241b855921cc47772493a7ecfe
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-11-04 12:13:09 -0300

    feat(nvim): save queries to custom location

--- .config/nvim/lua/extra/plugins/database.lua
+++ .config/nvim/lua/extra/plugins/database.lua
@@ -5,7 +5,6 @@ return {
     { "kristijanhusak/vim-dadbod-completion", ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
     "hrsh7th/nvim-cmp"
   },
-  event = "VeryLazy",
   cmd = {
     'DBUI',
     'DBUIToggle',
@@ -14,6 +13,7 @@ return {
   },
   config = function()
     vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_save_location = '~/.scripts/work/loggi/redshift/'
     require('cmp').setup.buffer({ sources = { { name = 'vim-dadbod-completion' } } })
   end
 }
