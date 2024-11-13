b73220db18dffb8e0d7ff19286d85be485f1b49b
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-11-13 12:46:36 -0300

    feat(gh): configure script and dash

--- bin/gh-loggi.sh
+++ bin/gh-loggi.sh
@@ -5,18 +5,23 @@
 FRONT_REPO="/opt/loggi/front-clientes-app-web/"
 BEYOND_PY_REPO="/opt/loggi/py/apps/beyond/"
 BEYOND_PAYMENTS_REPO="/opt/loggi/beyond-payment/"
+LWEB_REPO="/opt/loggi/web/"
 
 PRS_FRONT=$(
   cd $FRONT_REPO || exit
-  gh pr list --state open --search 'review-requested:@me' --json author,title --jq 'length'
+  gh pr list --search 'is:open status:success review-requested:@me' --json author,title --jq 'length'
 )
 PRS_BEYOND_PY=$(
   cd $BEYOND_PY_REPO || exit
-  gh pr list --state open --search 'review-requested:@me' --json author,title --jq 'length'
+  gh pr list --search 'is:open status:success review-requested:@me' --json author,title --jq 'length'
 )
 PRS_BEYOND_PAYMENTS=$(
   cd $BEYOND_PAYMENTS_REPO || exit
-  gh pr list --state open --search 'review-requested:@me' --json author,title --jq 'length'
+  gh pr list --search 'is:open status:success review-requested:@me' --json author,title --jq 'length'
+)
+PRS_LWEB=$(
+  cd $LWEB_REPO || exit
+  gh pr list --search 'is:open status:success review-requested:@me' --json author,title --jq 'length'
 )
 
-echo "  |  $PRS_FRONT  $PRS_BEYOND_PY   $PRS_BEYOND_PAYMENTS"
+echo "  |  $PRS_FRONT  $PRS_BEYOND_PY   $PRS_BEYOND_PAYMENTS LW: $PRS_LWEB"
