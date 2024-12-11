f2ec5cd47166a56a99da1bf57899bf771d0f8888
Gustavo Santos <gustavo.fernandes@loggi.com>
2024-12-11 17:59:03 -0300

    feat(wezterm): update wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,21 +2,27 @@ local config = {}
 
 local wezterm = require("wezterm")
 require("navigation").setup()
--- require("keymapping").setup(config)
-require("statusline").setup()
+require("keymapping").setup(config)
+-- require("statusline").setup()
 
 
 config.automatically_reload_config = false
 config.force_reverse_video_cursor = true
 config.color_scheme = "Kanagawa"
 
--- config.font = wezterm.font("MonoLisa Nerd Font")
-config.font = wezterm.font("BerkeleyMono Nerd Font")
+config.font = wezterm.font("MonoLisa Nerd Font")
+-- config.font = wezterm.font_with_fallback({
+--     { family = "OperatorMono Nerd Font", weight = "Regular" },
+--     "JetBrains Mono",
+--     "Noto Color Emoji",
+--     "Symbols Nerd Font Mono",
+-- })
+-- config.font = wezterm.font("BerkeleyMono Nerd Font")
 config.font_size = 12
 config.max_fps = 120
-config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
 
-config.window_decorations = "RESIZE"
+config.window_decorations = "TITLE | RESIZE"
 config.window_background_opacity = 1.0
 config.window_close_confirmation = "NeverPrompt"
 
